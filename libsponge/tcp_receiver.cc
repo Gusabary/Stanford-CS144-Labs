fe038aaeef07d01e22ec8bda58d2f6bcf443d543
Gusabary <gusabary@126.com>
2020-09-10 12:08:28 +0000

    feat(lab2): finish TCP receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,10 +11,46 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 bool TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
-    return {};
+    const TCPHeader header = seg.header();
+    if (!_isn.has_value()) {
+        if (header.syn) {
+            _isn = header.seqno;
+            _ackno = _isn.value();
+        } else {
+            return false;
+        }
+    }
+
+    bool ret = true;
+    const uint64_t unwrapped_seqno = unwrap(header.seqno, _isn.value(), _checkpoint);
+    const uint64_t unwrapped_ackno = unwrap(_ackno, _isn.value(), _checkpoint);
+    if (unwrapped_seqno + seg.length_in_sequence_space() <= unwrapped_ackno ||
+        unwrapped_seqno >= unwrapped_ackno + window_size()) {
+        // no overlap between segment and window
+        ret = false;
+    }
+
+    const size_t size_before_push = stream_out().buffer_size();
+
+    const string payload = seg.payload().copy();
+    const size_t index = header.syn ? unwrapped_seqno : (unwrapped_seqno - 1);
+    _checkpoint = index;
+    const bool eof = header.fin;
+    _reassembler.push_substring(payload, index, eof);
+
+    const size_t size_after_push = stream_out().buffer_size();
+
+    if (header.syn) {
+        _ackno = _ackno + 1;
+    }
+    _ackno = _ackno + (size_after_push - size_before_push);
+    if (eof) {
+        _ackno = _ackno + 1;
+    }
+
+    return ret;
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { return _isn.has_value() ? optional<WrappingInt32>{_ackno} : nullopt; }
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - stream_out().buffer_size(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -19,13 +19,18 @@ class TCPReceiver {
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
+    
+    std::optional<WrappingInt32> _isn;
+    WrappingInt32 _ackno;
+    uint64_t _checkpoint;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) 
+      : _reassembler(capacity), _capacity(capacity), _isn(), _ackno(0), _checkpoint(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
